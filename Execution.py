fdaec00e89a9e05ccddd81fe03ea968e6440719b
wouter_vw <woutervweerelt@gmail.com>
2025-03-27 12:45:07 +0100

    Algorithm edit and Data Upload

--- Execution.py
+++ Execution.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import time
 import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
+import warnings
 ran = np.random.default_rng()
 
 from tvopt import utils
-from tools import TVQuadratic, online_gradient, online_gradient_control, control_design, sine_product_z_transform, polynomial_product
+from tools import TVQuadratic, online_gradient, online_gradient_control, control_design, sine_product_z_transform, polynomial_product, convergence_rate
 from SYSID import online_gradient_control_sys_id_ARM
 
 #%% SET-UP
@@ -16,7 +18,7 @@ save_data = False
 n = 15 # size of the unknown
 
 t_s = 0.1 # sampling time
-t_max = 200 # simulation length
+t_max = 500 # simulation length
 num_samples = int(t_max/t_s) # total number of iterations
 
 
@@ -41,7 +43,8 @@ x0 = 50*ran.normal(size=(n,1))
 
 #%% CHOOSE b_k
 
-b_type = "constant" # "ramp" "sine" "sine+ramp" "sine^2"
+b_type = "sine" # "ramp" "sine" "sine+ramp" "sine^2" "sine-sine" "sine^2-sine" "ramp-then-sine" "constant"
+A_type = "constant" # "constant" "time-varying"
 
 if b_type == "constant":
     different_systems = False
@@ -79,24 +82,60 @@ if b_type == "sine^2":
 
     coeffs_sys = polynomial_product([-1, 1], [1, -2*np.cos(omega*t_s), 1], [1, -2*np.cos(2*omega*t_s), 1])
 
-if b_type == "sine_inexact_B_D":
-    different_systems = False
-    omega = 1 # periodicity
-    b = np.sin(omega*np.arange(0,t_max,t_s)*np.ones((n,1)))
-    coeffs_sys = polynomial_product([-1, 1], [1, -2*np.cos(omega*t_s), 1], [1, -2*np.cos(2*omega*t_s), 1])
-
-
+if b_type == "sine-sine":
+    different_systems = True
+    omega1, omega2 = 1, 2
+    t1 = np.arange(0, t_max / 2, t_s)
+    t2 = np.arange(t_max / 2, t_max, t_s)
     
-# compute optimal trajectory (used to compute the tracking error ||x_k - x_k^*||)
-#x_opt = np.hstack([-la.solve(A,b[:,[k]]) for k in range(num_samples)])
-x_opt = np.hstack([-la.solve(A_list[k], b[:,[k]]) for k in range(num_samples)])
+    b1 = np.sin(omega1 * t1) * np.ones((n, 1))  # First half with omega = 1
+    b2 = np.sin(omega2 * t2) * np.ones((n, 1))  # Second half with omega = 2
+    
+    b = np.hstack((b1, b2))  # Concatenate along the time axis
 
+    coeffs_sys = sine_product_z_transform(([omega1*t_s]))
+    coeffs_sys_2 = sine_product_z_transform(([omega2*t_s]))
+
+if b_type == "sine^2-sine":
+    different_systems = True
+    omega1, omega2 = 1, 10
+    t1 = np.arange(0, t_max / 2, t_s)
+    t2 = np.arange(t_max / 2, t_max, t_s)
+    
+    b1 = np.sin(omega1 * t1) * np.ones((n, 1))  # sine
+    b2 = (np.sin(omega2 * t2) * np.ones((n,1)))**2  # sine^2
+    
+    b = np.hstack((b2, b1))  # Concatenate along the time axis
+
+    coeffs_sys = sine_product_z_transform(([omega1*t_s]))
+    coeffs_sys_2 = polynomial_product([-1, 1], [1, -2*np.cos(omega2*t_s), 1], [1, -2*np.cos(2*omega2*t_s), 1])
+
+if b_type == "ramp-then-sine":
+    different_systems = True
+    omega1 = 2
+    t1 = np.arange(0, t_max / 2, t_s)
+    t2 = np.arange(t_max / 2, t_max, t_s)
+    
+    b1 = np.sin(omega1 * t2) * np.ones((n, 1))  # sine
+    b_bar = 5*ran.random((n,1)) # velocity
+    b2 = t1 *b_bar + b_bar
+
+    b = np.hstack((b2, b1))  # Concatenate along the time axis
+    
+    coeffs_sys_2 = sine_product_z_transform(([omega1*t_s]))
+    coeffs_sys = [1, -2, 1]
 
 # generate cost function
 b_list = [b[:,[k]] for k in range(b.shape[1])]
-f = TVQuadratic(A_list, b_list, t_s=t_s)
 
-os.makedirs(f"data/{b_type}", exist_ok=True)
+if A_type == "constant":    
+    x_opt = np.hstack([-la.solve(A,b[:,[k]]) for k in range(num_samples)])
+    f = TVQuadratic(A, b_list, t_s=t_s)
+if A_type == "time-varying":
+    f = TVQuadratic(A_list, b_list, t_s=t_s)
+    x_opt = np.hstack([-la.solve(A_list[k], b[:,[k]]) for k in range(num_samples)])
+
+os.makedirs(f"data/{b_type}_{A_type}", exist_ok=True)
 
 
 # ----- online gradient (Nicola's code)
@@ -106,14 +145,16 @@ error_gradient = [la.norm(x[...,k]-x_opt[:,[k]]) for k in range(num_samples)]
 
 
 # ------ control theoretical approach (Nicola's code)
+
 print("Vanilla Approach Executing")
 coeffs_ctrl = control_design(coeffs_sys, [mu, L])
 x = online_gradient_control({"f":f, "b":coeffs_sys}, coeffs_ctrl, x_0=x0)
 error_control = [la.norm(x[...,k]-x_opt[:,[k]]) for k in range(num_samples)]
 
-# ------ control theoretical with SYS ID
-x = online_gradient_control_sys_id_ARM(f, [mu,L], e_k_best = 1e-6, delta = -0.13, f_factor = 0.1 , step = step, x_0=0)
-error_control_ARX = [la.norm(x[...,k]-x_opt[:,[k]]) for k in range(num_samples)]
+
+x, test_coeffs= online_gradient_control_sys_id_ARM(f, [mu,L], e_threshold = 1e-12, e_threshold2 = 1e-12, delta = -0.13, f_factor1 = 0.1, f_factor2 = 0.1, win_size1=4, win_size2=9, step=step, x_0=0)
+error_control_test = [la.norm(x[...,k]-x_opt[:,[k]]) for k in range(num_samples)]
+
 
 markevery = 1
 fontsize = 16
@@ -124,9 +165,9 @@ t = np.arange(0,t_max,t_s)
 plt.figure()
 plt.semilogy(t, error_gradient, label="Online gradient")
 
-plt.semilogy(t, error_control_ARX, label="Control-based Sys ID ARX")
+plt.semilogy(t, error_control_test, label="Control-based Sys ID ARX")
 
-plt.semilogy(t, error_control, label="Control-based")
+plt.semilogy(t, error_control, label="Control-based Baseline")
 
 # ------ asymptotic error ------
 # computed as the maximum tracking error in the last 4/5ths of the simulation
@@ -135,6 +176,6 @@ tt = int(4*num_samples/5)
 plt.xlabel("Time", fontsize=fontsize)
 plt.ylabel("Tracking error", fontsize=fontsize)
 plt.legend(fontsize=fontsize-2)
-plt.title(rf"Online Optimization with $\boldsymbol{{b}}_{{k}}$ = {b_type} and Changing Hessian")
-plt.savefig(f"data/{b_type}/1-error_comparison-{b_type}.pdf", bbox_inches="tight")
+plt.title(rf"Online Optimization with $\boldsymbol{{b}}_{{k}}$ = {b_type} and {A_type} Hessian")
+plt.savefig(f"data/{b_type}_{A_type}/1-error_comparison-{b_type}_{A_type}.pdf", bbox_inches="tight")
 plt.show()
--- SYSID.py
+++ SYSID.py
@@ -3,7 +3,6 @@ from tools import control_design
 from tvopt import costs, sets
 import matplotlib.pyplot as plt
 
-## This is the function that performs RLS for given k
 def RLS_func(b_hat, P, f_factor, k, b, order):
     # Form the regressor vector with the order which is given at k given
     h_k = np.array([b[:, k - i - 1] for i in range(order)])
@@ -12,7 +11,7 @@ def RLS_func(b_hat, P, f_factor, k, b, order):
     e_k_bef = b[:, k].reshape((len(b),1)) + h_k.T @ b_hat  
 
     ### Reset the P matrix to preserve stability (This must be done sometimes as the signal may not excite RLS enough!
-    if np.linalg.norm(P) > 1e16:
+    if np.linalg.norm(P) > 1e12:
         P = np.eye(order) * 1 
     
     # Update RLS gain (In the case that we encounter a singular matrix we try a potential more stable calculation, in practice has not be activated. 
@@ -20,7 +19,7 @@ def RLS_func(b_hat, P, f_factor, k, b, order):
         K_k = P @ h_k @ np.linalg.inv(np.eye(len(b)) * f_factor + h_k.T @ P @ h_k)
     except np.linalg.LinAlgError:
         print("Error: Singular matrix encountered.")
-        K_k = P @ h_k @ np.linalg.inv(np.eye(len(b)) + h_k.T @ P @ h_k)
+        K_k = P @ h_k @ np.linalg.pinv(np.eye(len(b))* f_factor + h_k.T @ P @ h_k)
 
     # Update parameter estimate
     b_hat = b_hat - K_k @ e_k_bef
@@ -47,7 +46,7 @@ def window_error(b_hat, e_k_RLS, b, k, order, win_size):
     return win_err
 
 # Online gradient with built in RLS
-def online_gradient_control_sys_id_ARM(f, lambda_lims, e_k_best, delta, f_factor, step, x_0=1):
+def online_gradient_control_sys_id_ARM(f, lambda_lims, e_threshold, e_threshold2, delta, f_factor1, f_factor2, win_size1, win_size2, step, x_0=1):
     x = np.ones(f.dom.shape + (f.time.num_samples+1,))
     x[...,0] = x_0
     
@@ -60,58 +59,102 @@ def online_gradient_control_sys_id_ARM(f, lambda_lims, e_k_best, delta, f_factor
     
     e_hat_best = np.ones((order,1))
     threshold_reached = False
-    test_coeffs_prev = 0
-    
+    test_coeffs_prev = [0]
+    kbest = f.time.num_samples
+    offset = 0
+
+    test_coeffs = [1, 1]
+
+    dim_previous = -1
+
     for k in range(f.time.num_samples):
-        if threshold_reached == False:
-            order_prev = order
-            try:
-                if k <= 3:
-                    raise ValueError("k must be greater than 0")
-                
-                growth = np.log(k)**(1 + delta)
-                if np.isinf(growth):
-                    raise OverflowError("Growth value is too large")
-                order = int(growth)
-        
-            except (ValueError, OverflowError) as e:
-                None
-        
-            ## Make sure to extend our b_hat and P when the order grows (As done in ARM(inf))
-            if order != order_prev:
-                e_hat = np.vstack((e_hat, [[1]]))
-                P = np.pad(P, ((0, 1), (0, 1)), mode='constant', constant_values=0)
-                P[-1, -1] = 1
+        order_prev = order
+        try:
+            if k <= 3:
+                raise ValueError("k must be greater than 0")
             
-            y = x[...,k]
+            growth = np.log(k - offset)**(1 + delta)
+            if np.isinf(growth):
+                raise OverflowError("Growth value is too large")
+            order = int(growth)
+        except (ValueError, OverflowError) as e:
+            None
+    
+        ## Make sure to extend our b_hat and P when the order grows (As done in ARM(inf))
+        if order != 1 and order != order_prev:
+            e_hat = np.vstack((e_hat, [[1]]))
+            P = np.pad(P, ((0, 1), (0, 1)), mode='constant', constant_values=0)
+            P[-1, -1] = 1
+        
+        if threshold_reached == True:
+            if len(test_coeffs) != len(test_coeffs_prev) or np.any(test_coeffs != test_coeffs_prev):
+                krecomp = k 
+                print(f"Recompute at {k}")
+                try:
+                    c = control_design(test_coeffs, lambda_lims)  
+                except:
+                    print("No solution found")
+                    c = control_design(test_coeffs_prev, lambda_lims) 
+                    test_coeffs = test_coeffs_prev
+                    
+                test_coeffs_prev = test_coeffs
+                dim = len(test_coeffs)-1
+
+                if dim != dim_previous:
+                    y = [np.zeros(f.dom.shape) for _ in range(dim)]
+                    dim_previous = dim
+
+            e = - f.gradient(x[...,k], k*f.time.t_s)
+      
+            y = y[1:] + [-sum([test_coeffs[i]*y[i]/test_coeffs[dim] for i in range(dim)]) + e/test_coeffs[dim]]
+            
+            x[...,k+1] = sum([c[i]*y[i] for i in range(dim)])
+
+            x_trial[...,k] = x[...,k+1].reshape(-1)
+
+            e_hat, P, e_k_RLS = RLS_func(e_hat, P, f_factor2, k, x_trial, order)
+            e_k_norm = window_error(e_hat, e_k_RLS, x_trial, k-2, order, win_size=win_size2)
+
+            if e_k_norm < e_k_best:
+                e_k_best = e_k_norm
+                e_hat_best = e_hat
+                kbest = k
+
+            if kbest + 20 == k:
+                test_coeffs = np.append(e_hat_best[::-1], 1.0)
+
+            if window_error(e_hat_best, 0, x_trial, k, len(e_hat_best), win_size=2) > window_error(e_hat_best, 0, x_trial, k-1, len(e_hat_best), win_size=4)*1e2 and k > krecomp + 20: 
+                threshold_reached = False
+                print(f"Reset sys id at {k}")
+                order = 1
+                order_prev = 1
+                dim = 0
+                dim_previous = -1
+                y = [np.zeros(f.dom.shape) for _ in range(order)]
+                e_hat = np.ones((order,1))
+                e_threshold = e_threshold2
+                P = np.eye(order) * 1
+                c = np.ones(order+1)
+                offset = k - 3
+            
+        if threshold_reached == False and k != offset:
+            y_unc = x[...,k]
             
             for _ in range(1):
             
-                y = y - step*f.gradient(y, k*f.time.t_s)
+                y_unc = y_unc - step*f.gradient(y_unc, k*f.time.t_s)
             
-            x[...,k+1] = y
+            x[...,k+1] = y_unc
         
             x_trial[...,k] = x[...,k+1].reshape(-1)
             
-            e_hat, P, e_k_RLS = RLS_func(e_hat, P, f_factor, k, x_trial, order)
-            e_k_norm = window_error(e_hat, e_k_RLS, x_trial, k-2, order, win_size=4)
+            e_hat, P, e_k_RLS = RLS_func(e_hat, P, f_factor1, k, x_trial, order)
+            e_k_norm = window_error(e_hat, e_k_RLS, x_trial, k-2, order, win_size=win_size1)
             
-            if e_k_norm < e_k_best:
-                e_k_best = e_k_RLS
+            if e_k_norm < e_threshold:
                 e_hat_best = e_hat
                 test_coeffs = np.append(e_hat_best[::-1], 1.0)
                 threshold_reached = True
-
-        if threshold_reached == True:
-            if np.any(test_coeffs != test_coeffs_prev):
-                c = control_design(test_coeffs, lambda_lims)  
-                test_coeffs_prev = test_coeffs
-                y = [np.zeros(f.dom.shape) for _ in range(order)]
+                e_k_best = e_threshold
                 
-            e = - f.gradient(x[...,k], k*f.time.t_s)
-                    
-            y = y[1:] + [-sum([test_coeffs[i]*y[i]/test_coeffs[order] for i in range(order)]) + e/test_coeffs[order]]
-            
-            x[...,k+1] = sum([c[i]*y[i] for i in range(order)])
-
-    return x
+    return x, test_coeffs
